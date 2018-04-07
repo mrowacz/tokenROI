b69b552152378fcafb963ffa0bd159d9cf7a0e81
Łukasz Czerwiński <mrowacz@gmail.com>
2018-04-07 23:52:31 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "tokenROI",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Lukasz Czerwinski",
     author_email = "mrowacz@gmail.com",
     description = ("Quick ROI calculator for your token balances base on"
