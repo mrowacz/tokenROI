18a827650e1e4da4600dcd74d355655388884e92
Łukasz Czerwiński <mrowacz@gmail.com>
2018-04-07 23:45:41 +0200

    doc: Add bumpversion
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "tokenROI",
+    version = "0.1.0",
+    author = "Lukasz Czerwinski",
+    author_email = "mrowacz@gmail.com",
+    description = ("Quick ROI calculator for your token balances base on"
+                   " idex.market prices"),
+    license = "MIT",
+    keywords = "tokens cryptocurrencies idex roi",
+    packages=['python', 'tests'],
+    install_requires=['python-editor', 'pydrive']
+)
