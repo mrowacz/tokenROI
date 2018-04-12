a156efe7c634b1dcae304bbcb8eb7392d8e0ba01
Łukasz Czerwiński <mrowacz@gmail.com>
2018-04-12 23:23:57 +0200

    app: Remove settings.yml
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "tokenROI",
-    version = "0.5.0",
+    version = "0.5.1",
     author = "Lukasz Czerwinski",
     author_email = "mrowacz@gmail.com",
     description = ("Quick ROI calculator for your token balances base on"
--- token_roi/config.py
+++ token_roi/config.py
@@ -3,7 +3,6 @@ import os
 CONF_PATH = '/.local/token/'
 TOKEN_CONF = 'token_list.txt'
 WALLETS_CONF = 'eth_wallets.txt'
-SETTINGS_CONF = 'settings.yaml'
 TOKEN_CONF_CONTENT = """# token_roi tokens configuration file
 # <TOKEN ID> <YOUR ICO PRICE> <AMOUNT OF COINS>
 # for example
@@ -12,15 +11,6 @@ WPR 0.000125 1000
 WALLETS_CONF_CONTENT = """# token_roi tokens configuration file
 # <YOUR ETH ADDRESS>
 0x2a0c0DBEcC7E4D658f48E01e3fA353F44050c208
-"""
-
-SETTINGS ="""
-'client_config_backend': 'file'
-'client_config_file': '/home/{}/.local/token/client_secrets.json'
-'save_credentials': True
-'save_credentials_backend': 'file'
-'save_credentials_file': '/home/{}/.local/token/creds'
-'oauth_scope': ['https://www.googleapis.com/auth/drive']
 """
 
 
@@ -36,7 +26,3 @@ def token_config_path():
 
 def wallet_config_path():
     return config_dir() + WALLETS_CONF
-
-
-def setting_config_path():
-    return config_dir() + SETTINGS_CONF
